d0977b560d69803899fb24ec52c2332db3d11127
unknown <C00301121@setu.ie>
2025-09-15 12:50:50 +0100

    added card drag effects

---
+++ scripts/card.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+signal hovered
+signal hovered_off
+
+func _ready() -> void:
+	get_parent().connect_card_signals(self) 	# All cards must be child of CardManager or error will occur
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_mouse_entered() -> void:
+	emit_signal("hovered", self)
+
+
+func _on_area_2d_mouse_exited() -> void:
+	emit_signal("hovered_off", self)
