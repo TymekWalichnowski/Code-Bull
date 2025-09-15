42ea90e2dff5c91476dcb618e91df900d2cbb348
unknown <C00301121@setu.ie>
2025-09-15 12:53:32 +0100

    fixed card dragging crash bug

--- scripts/card_manager.gd
+++ scripts/card_manager.gd
@@ -24,7 +24,8 @@ func _input(event):
 			if card:
 				start_drag(card)
 		else:
-			finish_drag()
+			if card_being_dragged:
+				finish_drag()
 
 func start_drag(card):
 	card.get_parent().move_child(card,-1) # taken from comment, may be buggy, makes sure cards fall on top of other cards
