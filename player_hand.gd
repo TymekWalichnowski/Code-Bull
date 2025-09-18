a2f465464780a03fd8390e2bbdde7547037ad327
unknown <C00301121@setu.ie>
2025-09-18 13:21:45 +0100

    added card deck

--- player_hand.gd
+++ player_hand.gd
@@ -1,38 +1,30 @@
 extends Node2D
 
-const CARD_SCENE_PATH = "res://scenes/card.tscn"
 const CARD_WIDTH = 150
 const HAND_Y_POSITION = 900
+const DEFAULT_CARD_MOVE_SPEED = 0.1
 
-var hand_count = 5
 var player_hand = []
 var center_screen_x
 
 func _ready() -> void:
 	center_screen_x = get_viewport().size.x / 2
-	var card_scene = preload(CARD_SCENE_PATH)
-	for i in range(hand_count):
-		var new_card = card_scene.instantiate()
-		$"../CardManager".add_child(new_card)
-		new_card.name = "Card"
-		add_card_to_hand(new_card)
-		
 
-func add_card_to_hand(card):
+func add_card_to_hand(card, speed):
 	if card not in player_hand:
 		player_hand.insert(0, card)
-		update_hand_positions()
+		update_hand_positions(speed)
 	else:
-		animate_card_to_position(card, card.hand_position)
+		animate_card_to_position(card, card.hand_position, speed)
 
 
-func update_hand_positions():
+func update_hand_positions(speed):
 	for i in range(player_hand.size()):
 		# get bew card position based on index
 		var new_position = Vector2(calculate_card_position(i), HAND_Y_POSITION)
 		var card = player_hand[i]
 		card.hand_position = new_position
-		animate_card_to_position(card, new_position)
+		animate_card_to_position(card, new_position, speed)
 
 func calculate_card_position(index):
 	var x_offset = (player_hand.size() -1) * CARD_WIDTH
@@ -40,14 +32,14 @@ func calculate_card_position(index):
 	return x_position
 	
 
-func animate_card_to_position(card, new_position):
+func animate_card_to_position(card, new_position, speed):
 	var tween = get_tree().create_tween()
-	tween.tween_property(card, "position", new_position, 0.1)
+	tween.tween_property(card, "position", new_position, speed)
 
 func remove_card_from_hand(card):
 	if card in player_hand:
 		player_hand.erase(card)
-		update_hand_positions()
+		update_hand_positions(DEFAULT_CARD_MOVE_SPEED)
 
 func _process(delta: float) -> void:
 	pass
--- scripts/card_manager.gd
+++ scripts/card_manager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const COLLISION_MASK_CARD = 1
 const COLLISION_MASK_CARD_SLOT = 2
+const DEFAULT_CARD_MOVE_SPEED = 0.1
 
 var screen_size
 var card_being_dragged
@@ -11,6 +12,7 @@ var player_hand_reference
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	player_hand_reference = $"../PlayerHand"
+	$"../InputManager".connect("left_mouse_button_released", on_left_click_released)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,20 +21,13 @@ func _process(delta: float) -> void:
 		card_being_dragged.position = Vector2(clamp(mouse_pos.x, 0, screen_size.x), 
 											  clamp(mouse_pos.y, 0, screen_size.y))
 
-func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			var card = raycast_check_for_card() # checking for card
-			if card:
-				start_drag(card)
-		else:
-			if card_being_dragged:
-				finish_drag()
-
 func start_drag(card):
 	card.get_parent().move_child(card,-1) # taken from comment, may be buggy, makes sure cards fall on top of other cards
 	card_being_dragged = card
 	card.scale = Vector2(1,1)
+	var card_slot_found = raycast_check_for_card_slot()
+	if card_slot_found and card_slot_found.card_in_slot:
+		card_slot_found.card_in_slot = false
 
 func finish_drag():
 	card_being_dragged.scale = Vector2(1.05,1.05)
@@ -41,15 +36,20 @@ func finish_drag():
 		player_hand_reference.remove_card_from_hand(card_being_dragged)
 		#Card dropped in empty card slot
 		card_being_dragged.position = card_slot_found.position
-		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
 		card_slot_found.card_in_slot = true
 	else:
-		player_hand_reference.add_card_to_hand(card_being_dragged)
+		player_hand_reference.add_card_to_hand(card_being_dragged, DEFAULT_CARD_MOVE_SPEED)
 	card_being_dragged = null
 
 func connect_card_signals(card):
 	card.connect("hovered", on_hovered_over_card)
 	card.connect("hovered_off", on_hovered_off_card)
+
+
+func on_left_click_released():
+	if card_being_dragged:
+		finish_drag()
+		
 
 func on_hovered_over_card(card):
 	if !is_hovering_on_card:
