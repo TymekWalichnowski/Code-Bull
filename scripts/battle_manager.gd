92e2ef52f4117f8d559ed50e4b4aec5ec06200d1
TW <C00301121@setu.ie>
2026-01-14 14:39:39 +0000

    started turn order stuff

--- scripts/battle_manager.gd
+++ scripts/battle_manager.gd
@@ -23,16 +23,16 @@ func _on_end_turn_button_pressed() -> void:
 
 func opponent_turn():
 	# wait a bit
-	battle_timer.start()
-	await battle_timer.timeout
+	await wait(0.2)
 	
 	# Draw a card if the deck isn't empty
 	if %OpponentDeck.opponent_deck.size() != 0:
 		%OpponentDeck.draw_card()
-		battle_timer.start()
-		await battle_timer.timeout
-
-	await play_opponent_cards() # just picks highest id for now
+		await wait(1)
+	
+	if empty_opponent_card_slots.size() != 0:
+		await play_opponent_cards() # just picks highest id for now
+	
 	end_opponent_turn()
 
 func play_opponent_cards():
@@ -50,15 +50,16 @@ func play_opponent_cards():
 		var slot = empty_opponent_card_slots[0]
 
 		# Animate card to slot
-		var tween = get_tree().create_tween()
-		tween.tween_property(card_to_play, "position", slot.position, CARD_MOVE_SPEED)
-		tween.tween_property(card_to_play, "scale", Vector2(SMALLER_CARD_SCALE, SMALLER_CARD_SCALE), CARD_MOVE_SPEED)
+		var tween1 = get_tree().create_tween()
+		tween1.tween_property(card_to_play, "position", slot.position, CARD_MOVE_SPEED)
+		var tween2 = get_tree().create_tween()
+		tween2.tween_property(card_to_play, "scale", Vector2(SMALLER_CARD_SCALE, SMALLER_CARD_SCALE), CARD_MOVE_SPEED)
 
 		if card_to_play.has_node("AnimationPlayer"):
 			card_to_play.get_node("AnimationPlayer").play("card_flip")
 
 		# Wait for animation to finish
-		await tween.finished
+		await tween1.finished
 
 		# Assign card to slot
 		card_to_play.cards_current_slot = slot
@@ -83,5 +84,29 @@ func _sort_card_descending(a, b):
 		return 1
 
 func end_opponent_turn():
+	%PlayerDeck.reset_draw()
+	start_clash()
+	
+func start_clash():
+	#attack()
 	$"../EndTurnButton".disabled = false
 	$"../EndTurnButton".visible = true
+	
+
+func attack(attacking_card, attacker):
+	var new_pos_y
+	if attacker == "Opponent":
+		new_pos_y = 1080
+	else:
+		new_pos_y = 0
+	
+	var new_pos = Vector2(attacking_card.position.x, new_pos_y)
+	var tween = get_tree().create_tween()
+	tween.tween_property(attacking_card, "position", new_pos, CARD_MOVE_SPEED)
+
+	print("activate card")
+
+func wait(wait_time):
+	battle_timer.wait_time = wait_time
+	battle_timer.start()
+	await battle_timer.timeout
